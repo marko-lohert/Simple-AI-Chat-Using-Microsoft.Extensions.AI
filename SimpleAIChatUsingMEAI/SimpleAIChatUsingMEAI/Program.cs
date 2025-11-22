8ff8d4948e0505dcd0c16b5829a3eb4c96d8564f
marko-lohert <lohert.marko@gmail.com>
2025-11-22 22:43:57 +0100

    A very simple chat implemented with Microsoft.Extensions.AI (a basic chat implementation). User can ask only one question.
    *This app does not obey Asimov's laws of robotics*

---
+++ SimpleAIChatUsingMEAI/SimpleAIChatUsingMEAI/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+IChatClient chatClient = new OllamaApiClient("http://localhost:11434", "gemma3:4b");
+
+Console.Write("> ");
+string prompt = Console.ReadLine() ?? string.Empty;
+ChatResponse response = await chatClient.GetResponseAsync(prompt);
+
+Console.WriteLine(response.Text);
