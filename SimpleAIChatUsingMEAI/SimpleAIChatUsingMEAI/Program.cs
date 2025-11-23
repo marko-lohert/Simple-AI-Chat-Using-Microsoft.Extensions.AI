514c5f727d4ce4a854e4fb0a93d7b754b592916c
marko-lohert <lohert.marko@gmail.com>
2025-11-23 22:50:05 +0100

    Add the ability to ask multiple questions (in a while loop), and also maintain chat history so that the AI has context for the conversation. If the user enters '/bye', the chat will exit.

--- SimpleAIChatUsingMEAI/SimpleAIChatUsingMEAI/Program.cs
+++ SimpleAIChatUsingMEAI/SimpleAIChatUsingMEAI/Program.cs
@@ -2,10 +2,25 @@
 using OllamaSharp;
 
 IChatClient chatClient = new OllamaApiClient("http://localhost:11434", "gemma3:4b");
+List<ChatMessage> ChatHistory = new();
 
-Console.Write("> ");
-string prompt = Console.ReadLine() ?? string.Empty;
-await foreach(ChatResponseUpdate responseUpdate in chatClient.GetStreamingResponseAsync(prompt))
+while (true)
 {
-    Console.Write(responseUpdate.Text);
+    Console.Write("> ");
+    string prompt = Console.ReadLine() ?? string.Empty;
+    if (prompt.Equals("/bye", StringComparison.OrdinalIgnoreCase))
+        return;
+
+    ChatHistory.Add(new ChatMessage(ChatRole.User, prompt));
+    List<ChatResponseUpdate> completeResponse = [];
+
+    await foreach (ChatResponseUpdate responseUpdate in chatClient.GetStreamingResponseAsync(ChatHistory))
+    {
+        Console.Write(responseUpdate.Text);
+        completeResponse.Add(responseUpdate);
+    }
+
+    ChatHistory.AddMessages(completeResponse);
+
+    Console.WriteLine();
 }
